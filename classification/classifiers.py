bb4f5cf00070f97c525e7b9cfd5eea8c5a7f4189
Lucas O'Conor <lvoconor@gmail.com>
2013-10-20 19:04:27 -0700

    Added feature vector extraction

--- classification/classifiers.py
+++ classification/classifiers.py
@@ -1,5 +1,16 @@
+#Stupid_Classifier is a binary classifier initialized with a list of words that are 
+#relevant to the positive label. If there are any matches with the good words in the 
+#test set then the classifier will return 1. Else it will return 0. 
+
 class Stupid_Classifier(Classifier)
-	def _init_()
-	def classify(text):
+	def _init_(goodwords):
+		self.goodwords = goodwords
+	#Classify looks at a word counter and goodwords
+	def classify(frequencies):
+		for w1 in frequencies:
+			for w2 in self.goodwords:
+				if w1 == w2: 
+					return 1
+		return 0
 
 
--- feature_extraction/dataloader.py
+++ feature_extraction/dataloader.py
@@ -1,19 +1,30 @@
 import csv
 
+# DataLoader loads a dictionary representing the raw csv data 
+class DataLoader:
+	def _init_(testfile):
+		self.raw_test_data = self.LoadTrainData(testfile)
+	def loadTrainData(filename):
+		raw_data = []
+		with open(filename, 'rb') as csvfile:
+			reader = csv.DictReader(csvfile, delimiter='\n')
+			for row in reader: 
+				raw_data.append(row)
+		return raw_data
+		
+		# returns an array of counters
+	def extractFeatureVectors():
+		features = []
+		for entry in self.raw_test_data:
+			text = entry['tweet']
+			features.append(Counter(text.split(' ')))
+		return features
+
+	def extractLabels():
+		labeldict={'sentiment':[], 'event':[], 'time':[]}
+		#for entry in self.raw.test_data:
 
 
-def LoadTrainData(filename):
-	tweets = []
-	labels = [] 
-	sentimentlabels = [] 
-	timelabels = [] 
-	weatherlabels = []
 
-	raw_data = []
-	with open(filename, 'rb') as csvfile:
-		reader = csv.DictReader(csvfile, delimiter='\n')
-		for row in reader: 
-			raw_data.append(row)
-	print raw_data
 
 LoadTrainData('train.csv')
