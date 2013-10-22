66b5b4e12c232ea217e6711975cd3609f8dbff13
Lucas O'Conor <lvoconor@gmail.com>
2013-10-21 21:30:39 -0700

    made changes to dataloader and added onevall classifier

--- classification/classifiers.py
+++ classification/classifiers.py
@@ -1,12 +1,58 @@
-#Stupid_Classifier is a binary classifier initialized with a list of words that are 
+#Stupid_Classifier is a bianry classifier initialized with a list of words that are 
 #relevant to the positive label. If there are any matches with the good words in the 
 #test set then the classifier will return 1. Else it will return 0. 
+class MultiClassClassifier(object):
+    def __init__(self, labels, classifiers):
+        """
+        @param list string: List of labels
+        @param list (string, Classifier): tuple of (label, classifier); the classifier is the one-vs-all classifier
+        """
+        # BEGIN_YOUR_CODE (around 2 lines of code expected)
+        self.labels = labels
+        self.classifiers = classifiers
+        # END_YOUR_CODE
 
-class Stupid_Classifier(Classifier)
-	def _init_(goodwords):
+    def classify(self, x):
+        """
+        @param string x: the text message
+        @return list (string, double): list of labels with scores 
+        """
+       
+
+    def classifyWithLabel(self, x):
+        """
+        @param string x: the text message
+        @return string y: one of the output labels
+        """
+        # BEGIN_YOUR_CODE (around 2 lines of code expected)
+        return max(self.classify(x), key=lambda x: x[1])[0]
+        # END_YOUR_CODE
+
+class OneVsAllClassifier(MultiClassClassifier):
+    def __init__(self, labels, classifiers):
+        """
+        @param list string: List of labels
+        @param list (string, Classifier): tuple of (label, classifier); the classifier is the one-vs-all classifier
+        """
+        super(OneVsAllClassifier, self).__init__(labels, classifiers)
+
+    def classify(self, x):
+        """
+        @param string x: the text message
+        @return list (string, double): list of labels with scores 
+        """
+        # BEGIN_YOUR_CODE (around 4 lines of code expected)
+        output = [] 
+        for c in self.classifiers:
+            output.append(c.classify(x))
+        return output
+        # END_YOUR_CODE
+
+class Stupid_Classifier(Classifier):
+	def __init__(self, goodwords):
 		self.goodwords = goodwords
 	#Classify looks at a word counter and goodwords
-	def classify(frequencies):
+	def classify(self, frequencies):
 		for w1 in frequencies:
 			for w2 in self.goodwords:
 				if w1 == w2: 
