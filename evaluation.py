5edc4af813a68cd5a9bb0beceb4d8fe2a0fc67db
Simon Zheng <simonzheng@stanford.edu>
2013-10-21 23:44:29 -0700

    reformated evaluatestupidclassifier.py

--- evaluation.py
+++ evaluation.py
@@ -1,3 +1,7 @@
+import numpy
+# from sklearn import cross_validation
+# from sklearn import datasets
+# from sklearn import svm
 import math, random
 
 # SAmple confidence vectors 
@@ -26,50 +30,95 @@ for gold in gold_list:
 # 	mean_squared_error = squared_error / numLabels
 # 	return mean_squared_error
 
-def single_data_point_se(predicted_confidence_vector, gold_confidence_vector):
-	numLabels = len(predicted_confidence_vector)
-	squared_error = 0.0
-	for label_index in range(numLabels):
-		squared_error += math.pow((predicted_confidence_vector[label_index] - gold_confidence_vector[label_index]), 2)
-	return squared_error
-
-def rmse(predictions_list, gold_list):
-	numPredictions = len(predictions_list)
-	if numPredictions <= 0: 
-		print "numPredictions <= 0"
-	if len(gold_list) <= 0:
-		print "len(gold_list) <= 0"
-	if numPredictions != len(gold_list):
-		print "predictions_list and gold_list do not match"
-
-	numLabels = len(predictions_list[0])
-	if numLabels <= 0:
-		print "prediction numLabels <= 0"
-	if len(gold_list[0]) <= 0:
-		print "gold numLabels <= 0"
-	if numLabels != len(gold_list[0]):
-		print "length of a prediction label vector and gold label vector do not match"
-
-	print "numPredictions is %d\nnumLabels is %d" %(numPredictions, numLabels)
-
-	# Note: ensure that numPredictions = len(gold_list) 
-	# Ensure that numPredictions > 0 and len(gold_list) > 0
-	# numLabels = len(gold_list[0])
-
-	total_squared_error = 0.0
-
-	for prediction_index in range(len(predictions_list)):
-		# total_mean_squared_error += single_data_point_mse(predictions_list[prediction_index], gold_list[prediction_index])
-		total_squared_error += single_data_point_se(predictions_list[prediction_index], gold_list[prediction_index])
-	total_mean_squared_error = total_squared_error / (numPredictions * numLabels)
-	root_mean_squared_error = math.sqrt(total_mean_squared_error)
-	return root_mean_squared_error
-
-print "rmse is %f" %(rmse(predictions_list, gold_list))
+class Evaluator:
+	def __init__(self, data):
+		self.data = data
 
 
-# Note: can copy or use scikit's: http://scikit-learn.org/stable/modules/cross_validation.html
+	def single_data_point_se(self, predicted_confidence_vector, gold_confidence_vector):
+		numLabels = len(predicted_confidence_vector)
+		squared_error = 0.0
+		for label_index in range(numLabels):
+			squared_error += math.pow((predicted_confidence_vector[label_index] - gold_confidence_vector[label_index]), 2)
+		return squared_error
 
-# def k_folds_validation(dataList):
-# 	numDataPoints = len(dataList)
-# 	numDataPointsPerFold = numDataPoints / 5 # Note: we can lose up to 4 total data numDataPoints
+	def rmse(self, predictions_list, gold_list):
+		numPredictions = len(predictions_list)
+		if numPredictions <= 0: 
+			print "numPredictions <= 0"
+			exit()
+		if len(gold_list) <= 0:
+			print "len(gold_list) <= 0"
+			exit()
+		if numPredictions != len(gold_list):
+			print "predictions_list and gold_list do not match in number of features"
+			exit()
+
+		numLabels = len(predictions_list[0])
+		if numLabels <= 0:
+			print "prediction numLabels <= 0"
+			exit()
+		if len(gold_list[0]) <= 0:
+			print "gold numLabels <= 0"
+			exit()
+		if numLabels != len(gold_list[0]):
+			print "length of a prediction label vector and gold label vector do not match"
+			exit()
+
+		print "numPredictions is %d\nnumLabels is %d" %(numPredictions, numLabels)
+
+		# Note: ensure that numPredictions = len(gold_list) 
+		# Ensure that numPredictions > 0 and len(gold_list) > 0
+		# numLabels = len(gold_list[0])
+
+		total_squared_error = 0.0
+
+		for prediction_index in range(len(predictions_list)):
+			# total_mean_squared_error += single_data_point_mse(predictions_list[prediction_index], gold_list[prediction_index])
+			total_squared_error += self.single_data_point_se(predictions_list[prediction_index], gold_list[prediction_index])
+		total_mean_squared_error = total_squared_error / (numPredictions * numLabels)
+		root_mean_squared_error = math.sqrt(total_mean_squared_error)
+		return root_mean_squared_error
+
+	# Note: can copy or use scikit's: http://scikit-learn.org/stable/modules/cross_validation.html
+
+	# def k_folds_validation(dataList):
+	# 	numDataPoints = len(dataList)
+	# 	numDataPointsPerFold = numDataPoints / 5 # Note: we can lose up to 4 total data numDataPoints
+
+def kfold_crossvalidate(dataList, k=5):
+	dataList = dataList[:len(dataList) - (len(dataList) % k)] # we effectively ignore the last len(dataList) % k data points
+	
+	if k > len(dataList):
+		print "your k = %d and is greater than the length of the dataList" % k
+		exit()
+
+	indices = numpy.random.permutation(len(dataList))
+	print "indices are ", indices
+	print "\n"
+	numDataPointsPerFold = len(dataList) / k
+	print "numDataPointsPerFold is ", numDataPointsPerFold
+
+	# we set aside 1/k of the data points for fold validation. 
+	for foldIndex in range(k):
+		training_idx = indices[foldIndex * numDataPointsPerFold : (foldIndex + 1) * numDataPointsPerFold]
+		test_idx = list(indices[:foldIndex * numDataPointsPerFold]) + list(indices[(foldIndex + 1) * numDataPointsPerFold:])
+		# print "training_idx is ", training_idx
+		# print "test_idx is ", test_idx
+
+		training, test = [dataList[index] for index in training_idx], [dataList[index] for index in test_idx]
+		# print "training is ", training
+		# print "test is ", test
+
+# evaluator = Evaluator()
+# print "rmse is %f" %(evaluator.rmse(predictions_list, gold_list))
+
+numpy.random.seed(0)
+numbers = ['a', 'b', 'c', 'd', 'e', 'f']
+kfold_crossvalidate(numbers)
+
+
+
+
+
+
