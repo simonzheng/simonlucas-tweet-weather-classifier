89316a6996d0a61be8fd11fa2ba6fa9a95806a1f
Lucas O'Conor <lvoconor@gmail.com>
2013-12-09 12:31:01 -0800

    Finished error analyis script and saved output to txt files

--- compareClassifiers.py
+++ compareClassifiers.py
@@ -10,6 +10,7 @@ loader =dataloader.DataLoader('data/train.csv')
 totalNumLabels = loader.totalNumLabels
 numDataPoints = loader.numDataPoints
 tweetcorpus = loader.corpus
+converter = vectorToLabel.Converter()
 
 
 
@@ -22,16 +23,70 @@ tweetcorpus = loader.corpus
 # snbc = structuredNaiveBayes.structuredNBClassifier(data_filename='data/train.csv', numFolds=0)
 # snbc_predictions_list = snbc.combined_classify_tweets(tweetsToBeTagged)
 
-converter = vectorToLabel.Converter()
 
-predictions = {'csp:', pickle.load(open('csppredicted.pkl')) , 'structurednb':pickle.load(open('csppredicted.pkl')), pickle.load(open('csppredicted.pkl'))]
-goldvectors = loader.extractFullLabelBitVectors
 
-for exampleindex in range(numDataPoints):
-	#if predictions['csp'][exampleindex] != predictions['structurednb']:
-	tweet = tweetcorpus[exampleindex]
-	print 'tweet'
-	print tweet
+def compare_combinedNB_csp(predictions, goldvectors):
+	for exampleindex in range(numDataPoints):
+		#if predictions['csp'][exampleindex] != predictions['structurednb']:
+		tweet = tweetcorpus[exampleindex]
+		truevalue = goldvectors[exampleindex]
+		cspprediction = predictions['csp'][exampleindex]
+		combinednbprediction = predictions['combinednb'][exampleindex]
+		if cspprediction != combinednbprediction:
+			cspcorrect = cspprediction == truevalue
+			combinednbcorrect = combinednbprediction == truevalue
+			print 'tweet:'
+			print tweet
+			print 'correct:'
+			if cspcorrect and combinednbcorrect:
+				print 'BOTH'
+			elif cspcorrect:
+				print 'CSP'
+			elif combinednbcorrect:
+				print 'COMBINEDNB'
+			else:
+				print 'NEITHER'
+			print 'true value'
+			print converter.convertToLabels(truevalue)
+			print 'csp prediction'
+			print converter.convertToLabels(cspprediction)
+			print 'combined nb prediction'
+			print converter.convertToLabels(combinednbprediction)
+def compare_structuredNB_combinedNB(predictions, goldvectors):
+	for exampleindex in range(numDataPoints):
+		#if predictions['csp'][exampleindex] != predictions['structurednb']:
+		tweet = tweetcorpus[exampleindex]
+		truevalue = goldvectors[exampleindex]
+		structurednbprediction = predictions['structurednb'][exampleindex]
+		combinednbprediction = predictions['combinednb'][exampleindex]
+		structuredlabels = converter.convertToLabels(structurednbprediction)
+		combinedlabels = converter.convertToLabels(combinednbprediction)
+		truelabels = converter.convertToLabels(truevalue)
+		if structuredlabels['event'] != combinedlabels['event']:
+			structurednbcorrect = structurednbprediction == truevalue
+			combinednbcorrect = combinednbprediction == truevalue
+			print 'tweet:'
+			print tweet
+			print 'correct:'
+			if structurednbcorrect and combinednbcorrect:
+				print 'BOTH'
+			elif structurednbcorrect:
+				print 'STRUCTUREDNB'
+			elif combinednbcorrect:
+				print 'COMBINEDNB'
+			else:
+				print 'NEITHER'
+			print 'true value'
+			print converter.convertToLabels(truevalue)
+			print 'structured nb prediction'
+			print converter.convertToLabels(structurednbprediction)
+			print 'combined nb prediction'
+			print converter.convertToLabels(combinednbprediction)
+
+
+predictions = {'csp': pickle.load(open('csppredicted.pkl')) , 'combinednb':pickle.load(open('combinedNBpredicted.pkl')), 'structurednb':pickle.load(open('structuredNBpredicted.pkl'))}
+goldvectors = loader.extractFullLabelBitVectors()
+compare_structuredNB_combinedNB(predictions, goldvectors)
 
 
 
